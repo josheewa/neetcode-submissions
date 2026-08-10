6e5af5df27ba7c137293921c52b945be14ed0169
josheewa <34496783+josheewa@users.noreply.github.com>
2026-08-10 08:02:24 -0400

    Update: three-integer-sum - submission-2

--- Data Structures & Algorithms/three-integer-sum/submission-2.py
+++ Data Structures & Algorithms/three-integer-sum/submission-2.py
@@ -1,26 +1,29 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums = sorted(nums)
-        d = defaultdict(int)
-        for x in nums:
-            d[x] += 1
         res = []
+
         for i in range(len(nums)):
-            d[nums[i]] -= 1
             if i > 0 and nums[i] == nums[i-1]:
                 continue
-            decs = []
-            for j in range(i+1, len(nums)):
-                d[nums[j]] -= 1
-                decs.append(nums[j])
-                if j > i + 1 and nums[j] == nums[j - 1]:
-                    continue
-                temp = -(nums[i] + nums[j])
+            if nums[i] > 0:
+                continue
+            j = i+1
+            k = len(nums)-1
 
-                if d[temp] > 0:
-                    res.append([nums[i], nums[j], temp])
-            for x in decs:
-                d[x] += 1
-
+            while j < k:
+                tsum = nums[i] + nums[j] + nums[k]
+                if tsum == 0:
+                    res.append([nums[i], nums[j], nums[k]])
+                    j += 1
+                    k -= 1
+                    while j < k and nums[j] == nums[j-1]:
+                        j += 1
+                    while j < k and nums[k] == nums[k+1]:
+                        k -= 1
+                elif tsum < 0:
+                    j += 1
+                else:
+                    k -= 1
+            
         return res
-                
