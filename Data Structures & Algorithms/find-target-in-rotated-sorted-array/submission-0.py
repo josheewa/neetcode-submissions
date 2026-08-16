cb329ef9a72ad839841568e0720aa5529fffa72c
josheewa <34496783+josheewa@users.noreply.github.com>
2026-08-16 11:44:03 -0400

    Update: find-target-in-rotated-sorted-array - submission-0

--- Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-0.py
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-0.py
@@ -8,20 +8,15 @@ class Solution:
             for x in (p, m, q):
                 if nums[x] == target: return x
 
-
             if nums[p] < nums[m] < nums[q]:
                 if target < nums[m]: q = m - 1
                 else: p = m + 1
             elif nums[p] < nums[m] > nums[q]:
                 if nums[p] < target < nums[m]: q = m - 1
                 else: p = m + 1
-                # if target > nums[m] or target < nums[p]: p = m + 1
             elif nums[p] > nums[m] < nums[q]:
                 if nums[m] < target < nums[q]: p = m + 1
                 else: q = m - 1
             else: 
                 return -1
-            
         return -1
-
-
