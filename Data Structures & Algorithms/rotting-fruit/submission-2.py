54042645926b4bad97ad8ab4b28426dfc42b0986
josheewa <34496783+josheewa@users.noreply.github.com>
2026-08-16 11:22:25 -0400

    Update: rotting-fruit - submission-2

--- Data Structures & Algorithms/rotting-fruit/submission-2.py
+++ Data Structures & Algorithms/rotting-fruit/submission-2.py
@@ -1,32 +1,27 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        m = len(grid)
-        n = len(grid[0])
-        dirs = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-        seen = [[False for _ in range(n)] for _ in range(m)]
-
+        dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
         q = deque()
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == 2:
-                    q.append([i,j])
+        fresh = 0
 
-        level = 0
-        while q:
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == 2: q.append((r, c))
+                elif grid[r][c] == 1: fresh += 1
+        
+        mins = 0
+        while q and fresh:
             for _ in range(len(q)):
                 r, c = q.popleft()
-                for i, j in dirs:
-                    nr = r + i
-                    nc = c + j
-                    if nr < 0 or nr >= m or nc < 0 or nc >= n: continue
-                    if seen[nr][nc] or grid[nr][nc] != 1: continue
-                    seen[nr][nc] = True
-                    grid[nr][nc] = 2
-                    q.append([nr, nc])
-            
-            if q: level += 1
-        
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == 1: return -1
-        return level
+
+                for dr, dc in dirs:
+                    nr = r + dr
+                    nc = c + dc
+                    if (0 <= nr < len(grid) 
+                    and 0 <= nc < len(grid[0])
+                    and grid[nr][nc] == 1):
+                        grid[nr][nc] = 2
+                        fresh -= 1
+                        q.append((nr, nc))
+            if q: mins += 1
+        return mins if fresh == 0 else -1
